1d37b5daa948b5c88599ac4bd5fe74c9db93f06d
Dante de Araujo <danteeng@hotmail.com>
2023-07-09 18:50:29 -0300

    Refatorando Create e Adicionando Mensagem de Contador

--- SMD-POO/AppPackage/TamagotchiFactory.java
+++ SMD-POO/AppPackage/TamagotchiFactory.java
@@ -4,27 +4,25 @@ public class TamagotchiFactory {
     static int count = 0;
 
     static Tamagotchi create(String type, String name){
-        count++;
+        Tamagotchi t = null;
         if(type.equals("duck")){
-            DuckTama quack = new DuckTama(name);
-            return quack;
+            t = new DuckTama(name);
         }
         else if(type.equals("dog")){
-            DogTama woof = new DogTama(name);
-            return woof;
+            t = new DogTama(name);
         }
         else if(type.equals("cat")){
-            CatTama meow = new CatTama(name);
-            return meow;
+            t = new CatTama(name);
         }
         else if(type.equals("butterfly")){
-            ButterflyTama flap = new ButterflyTama(name);
-            return flap;
+            t = new ButterflyTama(name);
         }
-        else{
-            System.out.println("Tama Invalido!");
-            count--;
-            return null;
+        else {
+			System.out.println("Tama Invalido!");
+			return null;
         }
+        count++;
+		System.out.printf("Ja nasceram um total de %d Mascotes Digitais no seu Mundo!\n", count);
+		return t;
     }
 }
