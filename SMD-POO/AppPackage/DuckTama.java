7f50c49a2576d2ac5a5b2a499b42da680b0f3987
Dante de Araujo <danteeng@hotmail.com>
2023-07-11 18:34:12 -0300

    Corrigindo hunger meter do pato

--- SMD-POO/AppPackage/DuckTama.java
+++ SMD-POO/AppPackage/DuckTama.java
@@ -6,7 +6,7 @@ public class DuckTama extends Tamagotchi {
       setFood("uva");
       this.health = new Meter(100,-0.5f);
       this.happiness = new Meter(90,0);
-      this.hunger = new Meter(0,10);
+      this.hunger = new Meter(0,1);
       this.tiredness = new Meter(0,-0.6f);
       this.poop = new Meter(0,-0.5f);
     }
