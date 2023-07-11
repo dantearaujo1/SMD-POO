e5b8c8cd2de3f310e20184089aaf7986b8069282
Dante de Araujo <danteeng@hotmail.com>
2023-07-11 18:35:05 -0300

    Corrigindo hunger meter do dog ê i ê

--- SMD-POO/AppPackage/DogTama.java
+++ SMD-POO/AppPackage/DogTama.java
@@ -7,7 +7,7 @@ public class DogTama extends Tamagotchi {
     	super(nome);
       setFood("carne");
       this.health = new Meter(100,0);
-      this.happiness = new Meter(100,5);
+      this.happiness = new Meter(100,0.5f);
       this.hunger = new Meter(0,0);
       this.tiredness = new Meter(0,0);
       this.poop = new Meter(0,0);
