d9816ab78f5685a54e4cfe7c3849488a02a63b45
Dante de Araujo <danteeng@hotmail.com>
2023-07-11 17:21:34 -0300

    Initializing Dog correct Attributes

--- SMD-POO/AppPackage/DogTama.java
+++ SMD-POO/AppPackage/DogTama.java
@@ -1,12 +1,20 @@
 package AppPackage;
 
+import AppPackage.states.NormalState;
+
 public class DogTama extends Tamagotchi {
     DogTama(String nome){
     	super(nome);
-        setFood("carne");
+      setFood("carne");
+      this.health = new Meter(100,0);
+      this.happiness = new Meter(100,5);
+      this.hunger = new Meter(0,0);
+      this.tiredness = new Meter(0,0);
+      this.poop = new Meter(0,0);
     }
 
     public void characteristic(){
-        System.out.println(", Au!");
+        System.out.println(this.getName() + " diz: Au!");
+        this.state.showState((Tamagotchi) this);
     }
 }
